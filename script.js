03b5c9c0651c44e11d6cc7247d5ba9800b76ce02
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-06 20:03:56 -0400

    trying to add a copy button

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 
 
-
 //for the drop down menu
 $(document).ready(function(){
-    $('select').formSelect();
-  });
+  $('select').formSelect();
+});
 
-  
+
 //for the modal
-  $(document).ready(function(){
-    $('.modal').modal();
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+//to make the button on modal copy the test
+$(function()  {
+  $('#copy-btn').click(function(){
+      var content = $('#full-information').html();
+      var newdiv = $("<div>");
+      newdiv.html(content);
+      $('#full-information').after(newdiv);
   });
+});
+
